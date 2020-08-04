75511e0ca3f41ed97a1f55381adf8bfb482cadcb
Paul Demorest <pdemores@nrao.edu>
2020-08-04 12:44:40 -0400

    Create database.txt in pulsar dir, needs fluxcal symlink.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='nanopipe',
-      version='1.0',
+      version='1.1',
       description='NANOGrav calibration and TOA pipeline',
       author='Paul Demorest',
       author_email='pdemores@nrao.edu',
