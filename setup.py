993bd5f0701564d2c27110be45b19da7b95c384d
Paul Demorest <pdemores@nrao.edu>
2021-02-11 13:46:11 -0500

    Version bump (1.2.1)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='nanopipe',
-      version='1.2.0',
+      version='1.2.1',
       description='NANOGrav calibration and TOA pipeline',
       author='Paul Demorest',
       author_email='pdemores@nrao.edu',
