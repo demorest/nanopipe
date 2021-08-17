513d86ead57b9da4492f5dbee37061c565b3f826
Paul Demorest <pdemores@nrao.edu>
2021-08-17 12:18:05 -0400

    Version 1.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='nanopipe',
-      version='1.2.3',
+      version='1.2.4',
       description='NANOGrav calibration and TOA pipeline',
       author='Paul Demorest',
       author_email='pdemores@nrao.edu',
@@ -13,5 +13,6 @@ setup(name='nanopipe',
       package_data={'nanopipe': ['data/psrjnames.dat','data/psrbnames.dat']},
       scripts=['scripts/make_psr_make', 'scripts/update_be_delay',
           'scripts/get_proper_name', 'scripts/fix_receiver_name',
-          'scripts/get_parfile_coords', 'scripts/psrindex']
+          'scripts/get_parfile_coords', 'scripts/psrindex',
+          'scripts/yuppi_psradd.py']
      )
